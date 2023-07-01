a5895d0ce1c8edb8c90457f1276612cafcd39b3d
Asada678 <asada.tomoya0923@gmail.com>
2023-07-01 09:49:52 +0900

    chore

--- asadatomoya-web/src/components/BackGround.tsx
+++ asadatomoya-web/src/components/BackGround.tsx
@@ -1,11 +1,11 @@
 import { type FC } from "react";
 
-interface BackGroundProps {}
+interface BackgroundProps {}
 
-const BackGround: FC<BackGroundProps> = ({}) => {
+const Background: FC<BackgroundProps> = ({}) => {
   return (
     <div className="bg-white-gradient dark:bg-black-gradient fixed left-0 top-0 -z-50 flex h-screen w-screen"></div>
   );
 };
 
-export default BackGround;
+export default Background;
