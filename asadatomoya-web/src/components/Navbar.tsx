3414d1c47cc4220e8ac42ecfc9cc7fd17cff7b66
Asada678 <asada.tomoya0923@gmail.com>
2023-07-08 21:09:04 +0900

    fix

--- asadatomoya-web/src/components/Navbar.tsx
+++ asadatomoya-web/src/components/Navbar.tsx
@@ -27,24 +27,24 @@ const Navbar: FC<NavbarProps> = ({}) => {
   const { viewport } = useViewport();
 
   useEffect(() => {
-    if (viewport.width === 0) return;
+    // if (viewport.width === 0) return;
     const context = gsap.context(() => {
       ScrollTrigger.create({
         trigger: asadatomoya.current,
         start: "center top",
         markers: true,
         onEnter() {
-          if (viewport.isMobile) {
-            gsap.to(asadatomoya.current, {
-              duration: 0.3,
-              opacity: 0,
-              x: -10,
-            });
-            gsap.to(logo.current, {
-              duration: 0.3,
-              x: 10,
-            });
-          }
+          // if (viewport.isMobile) {
+          gsap.to(asadatomoya.current, {
+            duration: 0.3,
+            opacity: 0,
+            x: -10,
+          });
+          gsap.to(logo.current, {
+            duration: 0.3,
+            x: 10,
+          });
+          // }
           setIsTop(false);
         },
         onEnterBack() {
@@ -70,7 +70,7 @@ const Navbar: FC<NavbarProps> = ({}) => {
     return () => {
       context.revert();
     };
-  }, [viewport]);
+  }, []);
 
   const toggleMobileMenu = () => setShowMobileMenu(!showMobileMenu);
 
